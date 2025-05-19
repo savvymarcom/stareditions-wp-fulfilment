8c192d9bb7bbc0e2caf5222b1725cf1f2fc3d53b
savvymarcom <robin@savvymarcom.com>
2025-05-19 10:55:30 +0100

    update emailer

--- includes/Service/EmailService.php
+++ includes/Service/EmailService.php
@@ -125,7 +125,8 @@ class EmailService
 
         $emailContent = $this->emailTemplate($emailTitle, $emailHeading, $emailBody);
         
-        $success = WC()->mailer()->send($this->adminEmail, $subject, $emailContent, $this->headers);        
+        //$success = WC()->mailer()->send($this->adminEmail, $subject, $emailContent, $this->headers);
+        $success = wp_mail($this->adminEmail, $subject, $emailContent, $this->headers);        
 
         if (!$success) {
             error_log('[SavvyWebPlugin] ❌ wp_mail failed to send.');
