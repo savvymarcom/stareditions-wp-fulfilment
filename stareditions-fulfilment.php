ff803c53a80f4e1bec7f79d864e60c86ac75aec8
savvymarcom <robin@savvymarcom.com>
2025-05-19 08:31:48 +0100

    update version

--- stareditions-fulfilment.php
+++ stareditions-fulfilment.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Star Editions Fulfilment
  * Description:     Custom fulfilment integration for Star Editions.
- * Version:         1.3.23
+ * Version:         1.3.24
  * Tested up to:    6.8.1
  * Author:          SavvyWeb Solutions
  */
